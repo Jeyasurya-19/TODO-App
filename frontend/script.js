799b3438c4ed6f9a85d67bc9583b2e8d86453c48
Jeyasurya-19 <jeya41140@gmail.com>
2026-08-19 16:52:14 +0530

    button

--- frontend/script.js
+++ frontend/script.js
@@ -124,3 +124,28 @@ logout.addEventListener("click",()=>{
     window.location.href="login.html";
 });
 loadTasks();
+// =========================================================
+// LIGHT / DARK MODE
+// =========================================================
+
+const themeToggle = document.querySelector("#themeToggle");
+
+if (themeToggle) {
+
+    themeToggle.addEventListener("click", () => {
+
+        document.body.classList.toggle("dark");
+
+        if (document.body.classList.contains("dark")) {
+
+            themeToggle.textContent = "☀️";
+
+        } else {
+
+            themeToggle.textContent = "🌙";
+
+        }
+
+    });
+
+}
